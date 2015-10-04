f7f7639fded1fdd2d5553b87610409ddf110b38f
maket <nmunce@gmail.com>
2015-10-04 17:13:54 -0400

    finished code generation; started UI

--- blockly/generators/javascript/brainfuck.js
+++ blockly/generators/javascript/brainfuck.js
@@ -1,92 +1,64 @@
 Blockly.JavaScript['brack'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "[" + Blockly.JavaScript.statementToCode(block, 'NAME') + "]";
+    return code;
 };
 
 Blockly.JavaScript['pt_inc'] = function(block) {
-    return ">";
+    var code = ">" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['pt_inc_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = ">" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['pt_dec'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "<" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['pt_dec_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = "<" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bt_inc'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "+" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bt_inc_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = "+" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bt_dec'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "-" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bt_dec_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = "-" + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bf_out'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "." + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bf_out_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = "." + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bf_in'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  return code;
+    var code = "," + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
 
 Blockly.JavaScript['bf_in_c'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+    var code = "," + Blockly.JavaScript.statementToCode(block, 'NAME');
+    return code;
 };
