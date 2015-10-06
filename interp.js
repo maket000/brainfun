f062e7ba7c5bc595f5111e4578724b416921b36f
maket <nmunce@gmail.com>
2015-10-05 23:54:21 -0400

    Added a neat array visualization thing, totally untested, and also needs like delay and stuff.

--- interp.js
+++ interp.js
@@ -2,17 +2,20 @@
 // Larissa Ulitsky
 // Sunday, October 4, 2015
 
+ARRAY_LEN = 30000
+var mem = [];
+
 function incMP(ptr, inc) {
 	var mp = ptr;
 	mp += inc;
 	
 	while (mp < 0) {
 		console.log("N");
-		mp += 30000;
+		mp += ARRAY_LEN;
 	}
 	while (mp >= 30000) {
 		console.log("M");
-		mp -= 30000;
+		mp -= ARRAY_LEN;
 	}
 	
 	return mp;
@@ -60,18 +63,16 @@ function findBlockEnd(pgm, pp) {
 	return ptr - 1;
 }
 
-function interp(pgm, inp) {
+function interp(pgm, inp, delay) {
 	var pp = 0;                  // program pointer
 	var pp_stack = [];           // stack of program pointer values, for looping
 	var sp = 0;                  // stack pointer for pp_stack
 	var pl = pgm.length          // length of program
 	var ip = 0;                  // input pointer
-	var mem = [];                // memory array
 	var mp = 0;                  // memory pointer
 	var out = ""                 // output string, NOT ACTUALLY NEEDED
 	
-	mem[30000] = 0;
-	for (i=0; i<30000; i++) {
+	for (i=0; i<ARRAY_LEN; i++) {
 		mem[i] = 0;
 	}
 	
@@ -80,26 +81,26 @@ function interp(pgm, inp) {
 		switch (exp) {
 			case '>':  // increment memory pointer
 				mp = incMP(mp, 1);
-//				move_ptr(mp);
+				movePtr(mp);
 			break;
 			
 			case '<':  // decrement memory pointer
 				mp = incMP(mp, -1);
-//				move_ptr(mp);
+				movePtr(mp);
 			break;
 			
 			case '+':  // increment memory value
 				mem[mp] = incMem(mem[mp], 1);
-//				change_val(mp, mem[mp]);
+				changeVal(mp, mem[mp]);
 			break;
 			
-			case '-':  // decrement memory value
+			case '-':  // decrement memorynnnn value
 				mem[mp] = incMem(mem[mp], -1);
-//				change_val(mp, mem[mp]);
+				changeVal(mp, mem[mp]);
 			break;
 			
 			case '.':  // send output character
-//				put_out(String.fromCharCode(mem[mp]));
+				printOut(String.fromCharCode(mem[mp]));
 				out += String.fromCharCode(mem[mp]);
 			break;
 			
@@ -107,7 +108,7 @@ function interp(pgm, inp) {
 				mem[mp] = incMem(mem[mp], -1 * mem[mp]);
 				mem[mp] = incMem(mem[mp], getIn(inp, ip));
 				ip++;
-//				change_val(mp, mem[mp]);
+				changeVal(mp, mem[mp]);
 			break;
 			
 			case '[':  // open while loop
@@ -133,4 +134,22 @@ function interp(pgm, inp) {
 		pp++;
 	}
 	return out;
+}
+
+
+function movePtr(ind) {
+    mem_ind = (ind - 5) % ARRAY_LEN
+    for (var i=0; i<11; i++) {
+	mem_ind = (mem_ind + 1) % ARRAY_LEN;
+        document.getElementById("array").rows[0].cells[i].innerHTML = mem_ind;
+	document.getElementById("array").rows[1].cells[i].innerHTML = mem[mem_ind];
+    }
+}
+
+function changeVal(ind, val) {
+    document.getElementById("array").rows[1].cells[5].innerHTML = val;
+}
+
+function printOut(chr) {
+    document.getElementById("bf-out").innerHTML += chr;
 }
